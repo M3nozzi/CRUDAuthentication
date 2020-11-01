f040436902545572bc4add5176fe011f06b9fdad
Fabio Menozzi <f.menozzi@yahoo.com>
2020-11-01 13:39:36 -0300

    authentication is working

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
+router.use(authMiddleware);
+
 router.get('/', (request, response) => {
 
-    return response.json({ message: 'Logado!!!!' });
+    return response.json({ message: 'Logado!!!!', user: request.userId });
 });
 
 module.exports = app => app.use('/profiles', router);
